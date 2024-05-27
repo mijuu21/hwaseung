62d10ea72b5e434cd5e25f9b8fb4eaa702c0d1a6
하지영 <jyline02@gmail.com>
2024-05-27 15:24:57 +0900

    lang추가 및 최종 스크립트 완성

--- js/script.js
+++ js/script.js
@@ -1,26 +1,41 @@
 $(function () {
-  //GNB
+  // GNB
   const $window = $(window);
   const $header = $('#header');
   const $menu = $('.gnb > li');
   const $submenu = $('.submenu-wrap');
   const $banner = $('.banner-slide');
-  const duration = 300;
   const $btnMenu = $('.btn-menu');
+  const duration = 300;
 
   // 모바일
   const $btnMmenu = $('.btn-m-menu');
-  const $MSubmenu = $('.m-submenu-wrap');
+  const $mSubmenu = $('.m-submenu-wrap');
   const $dim = $('.dim');
   const $btnClose = $('.btn-close');
+  const $mGnbMenu = $('.m-gnb > li');
+  const $mGnbSubmenu = $('.m-gnb-sub');
+
+  // 모바일 용 메뉴를 클릭했을때
+  $mGnbMenu.on('click', function () {
+    $(this).toggleClass('on');
+    $(this).siblings().removeClass('on');
+    $(this).find($mGnbSubmenu).stop().slideToggle(duration);
+    $(this).siblings().find($mGnbSubmenu).stop().slideUp(duration);
+  });
 
   $btnMmenu.on('click', function () {
-    $MSubmenu.addClass('active');
+    $mSubmenu.addClass('active');
     $dim.fadeIn(duration);
   });
-  $btnClose.on('click', function () {
-    $MSubmenu.removeClass('active');
+
+  $btnClose.add($dim).on('click', function () {
+    $mSubmenu.removeClass('active');
     $dim.fadeOut(duration);
+
+    // 모바일 용 서브메뉴 초기화
+    $mGnbMenu.removeClass('on');
+    $mGnbMenu.stop().slideUp(duration);
   });
 
   // 마우스가 메뉴에 들어오면(mouseenter)
@@ -28,8 +43,10 @@ $(function () {
     const menuIdx = $(this).index();
     $menu.removeClass('on').eq(menuIdx).addClass('on');
     $submenu.find('li').removeClass('on').eq(menuIdx).addClass('on');
+
     openMenu();
   });
+
   // 마우스가 메뉴에 나가면(mouseleave)
   $header.on('mouseleave', function () {
     $menu.removeClass('on');
@@ -46,30 +63,38 @@ $(function () {
     $submenu.stop().fadeIn(duration);
     $banner.stop().fadeIn(duration);
   }
+
   function closeMenu() {
     $header.removeClass('active');
     $submenu.stop().fadeOut(duration);
     $banner.stop().fadeOut(duration);
   }
 
-  // 스크롤 이벤트
-  $window.on('scroll', function () {
-    // 얼마나 스크롤 되었는지 값을 구해서 저장
-    const scrollTop = $(this).scrollTop();
+  let scrollTop = $window.scrollTop();
+  const visualHeight = $('.visual').outerHeight();
+  setWhiteBackground();
 
-    // 비주얼 영역의 세로크기 저장
-    const visualHeight = $('.visual').outerHeight();
-    console.log(scrollTop, visualHeight);
-
-    // 두 값을 비교해서(스크롤값이 비주얼 영역의 세로보다 크다면)
+  function setWhiteBackground() {
     if (scrollTop >= visualHeight) {
       $header.addClass('w-bg');
     } else {
       $header.removeClass('w-bg');
     }
+  }
+  // 스크롤 이벤트
+  $window.on('scroll', function () {
+    // 얼마나 스크롤 되었는지 값을 구해서 저장
+    scrollTop = $(this).scrollTop();
+    setWhiteBackground();
   });
+
+  // 언어선택
+  $('.btn-lang').on('click', function () {
+    $('.lang-select').stop().$slideToggle(duration);
+  });
+
   // family site
-  $('family-site select').on('change', function () {
+  $('.family-site select').on('change', function () {
     const linkValue = $(this).val();
     window.open(linkValue);
   });
